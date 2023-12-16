9fab6a8d9775f8e26c441a7f582b1e7323094bab
Alena Hertman <gertmanea@mail.ru>
2023-12-16 21:02:45 +0300

    "дз готово"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,110 @@
 package org.example;
 
+import org.example.models.School;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
+
+    /**
+     * Задание
+     * =======
+     * Создайте базу данных (например, SchoolDB).
+     * В этой базе данных создайте таблицу Courses с полями id (ключ), title, и duration.
+     * Настройте Hibernate для работы с вашей базой данных.
+     * Создайте Java-класс Course, соответствующий таблице Courses, с необходимыми аннотациями Hibernate.
+     * Используя Hibernate, напишите код для вставки, чтения, обновления и удаления данных в таблице Courses.
+     * Убедитесь, что каждая операция выполняется в отдельной транзакции.
+     */
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //region Создание таблицы в БД без Hibernate
+//        String url = "jdbc:mysql://localhost:3306/";
+//        String user = "root";
+//        String password = "root";
+//        try(Connection connection = DriverManager.getConnection(url, user, password)){
+//            // Создание базы данных
+//            createDatabase(connection);
+//            System.out.println("Database created successfully");
+//
+//            // Использование базы данных
+//            useDatabase(connection);
+//            System.out.println("Use database successfully");
+//
+//            // Создание таблицы
+//            createTable(connection);
+//            System.out.println("Create table successfully");
+//
+//        }
+//        catch (SQLException e){
+//            e.printStackTrace();
+//        }
+        //endregion
+
+        //region Работа с Hibernate
+
+        try(SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(School.class)
+                .buildSessionFactory()){
+
+            // Создание сессии
+            Session session = sessionFactory.getCurrentSession();
+
+            // Начало транзакции
+            session.beginTransaction();
+
+            // Создание объекта
+            School group = School.create();
+            session.save(group);
+            System.out.println("Object group save successfully");
+
+            // Чтение объекта из базы данных
+            School retrievedStudent = session.get(School.class, group.getId());
+            System.out.println("Object student retrieved successfully");
+            System.out.println("Retrieved student object: " + retrievedStudent);
+
+            // Обновление объекта
+            retrievedStudent.updateTitle();
+            retrievedStudent.updateDuration();
+            session.update(retrievedStudent);
+            System.out.println("Object student update successfully");
+
+//            session.delete(retrievedStudent);
+//            System.out.println("Object student delete successfully");
+
+            session.getTransaction().commit();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        //endregion
     }
+
+
+    //region Вспомогательные методы
+
+//    private static void createDatabase(Connection connection) throws SQLException {
+//        String createDatabaseSQL =  "CREATE DATABASE IF NOT EXISTS schoolDB;";
+//        try (PreparedStatement statement = connection.prepareStatement(createDatabaseSQL)) {
+//            statement.execute();
+//        }
+//    }
+//
+//    private static void useDatabase(Connection connection) throws SQLException {
+//        String useDatabaseSQL =  "USE schoolDB;";
+//        try (PreparedStatement statement = connection.prepareStatement(useDatabaseSQL)) {
+//            statement.execute();
+//        }
+//    }
+//
+//    private static void createTable(Connection connection) throws SQLException {
+//        String createTableSQL = "CREATE TABLE IF NOT EXISTS Courses (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), duration DOUBLE);";
+//        try (PreparedStatement statement = connection.prepareStatement(createTableSQL)) {
+//            statement.execute();
+//        }
+    //endregion
 }
--- src/main/java/org/example/models/School.java
+++ src/main/java/org/example/models/School.java
@@ -1,2 +1,92 @@
-package org.example.models;public class School {
+package org.example.models;
+
+import javax.persistence.*;
+import java.util.Random;
+
+@Entity
+@Table(name = "Courses")
+public class School {
+
+    //region Поля
+
+    private static final String[] titles = new String[] { "Право", "Бух", "Эконом", "Програм", "Полит", "Ветеринар"};
+    private static final Double[] durations = new Double[] {1.2, 1.5, 2.0, 2.5};
+    private static final Random random = new Random();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "duration")
+    private double duration;
+
+    //endregion
+
+    //region Конструкторы
+    public School() {
+    }
+
+    public School(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public School(int id, String title, double duration) {
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+    }
+    //endregion
+
+    //region вспомогательные методы create updateDuration updateTitle
+    public static School create(){
+        return new School(titles[random.nextInt(titles.length)], durations[random.nextInt(durations.length)]);
+    }
+
+    public void updateDuration(){
+        duration = durations[random.nextInt(durations.length)];
+    }
+
+    public void updateTitle(){
+        title = titles[random.nextInt(titles.length)];
+    }
+    //endregion
+
+
+    //region Getter Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+    //endregion
+
+    @Override
+    public String toString() {
+        return "School {" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
 }
